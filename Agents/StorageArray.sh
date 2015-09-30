f09cf82a2e4fc637cfc80b5e0566de9f0ca73045
kseniya-buraya <ksu.nyaka@gmail.com>
2015-09-30 17:17:11 -0400

    test changes

--- Agents/StorageArray.sh
+++ Agents/StorageArray.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 
-ip netns exec ns1 ./Client_Server_Source/rpc_server srvns1 &
+ip netns exec ns1 ./CLI_SERV_SOURCE/rpc_server srvns1 service_1 &
 sleep 3
-./Client_Server_Source/rpc_client 192.168.16.135 "agent1 to srvns1"
+./CLI_SERV_SOURCE/rpc_client 192.168.16.135 service_1 "agent1 to service_1 from srvns1"
+ip netns exec ns1 rpcinfo -s
 
-ip netns exec ns2 ./Client_Server_Source/rpc_server srvns2 &
+ip netns exec ns2 ./CLI_SERV_SOURCE/rpc_server srvns2 service_2 &
 sleep 3
-./Client_Server_Source/rpc_client 192.168.16.136 "agent2 to srvns2"
+./CLI_SERV_SOURCE/rpc_client 192.168.16.136 service_2 "agent2 to service_2 from srvns2"
+ip netns exec ns2 rpcinfo -s
 
-ip netns exec ns1 ./Client_Server_Source/rpc_server srvns3 &
+ip netns exec ns1 ./CLI_SERV_SOURCE/rpc_server srvns3 service_3 &
 sleep 3
-./Client_Server_Source/rpc_client 192.168.16.135 "agent1 to srvns3"
+./CLI_SERV_SOURCE/rpc_client 192.168.16.135 service_3 "agent3 to service_3 from srvns3"
+ip netns exec ns1 rpcinfo -s
+
+./CLI_SERV_SOURCE/rpc_client 192.168.16.135 service_1 "agent1 to service_1 from srvns1"
 
 ./Agents/nsconfig.sh ns1 stop
 ./Agents/nsconfig.sh ns2 stop
