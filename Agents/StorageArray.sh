448bb6fd53b34212cda40667c2cc41437fb728d2
kseniya-buraya <ksu.nyaka@gmail.com>
2015-10-01 08:40:18 -0400

    test changes

--- Agents/StorageArray.sh
+++ Agents/StorageArray.sh
@@ -3,17 +3,14 @@
 ip netns exec ns1 ./CLI_SERV_SOURCE/rpc_server srvns1 service_1 tcp
 sleep 3
 ./CLI_SERV_SOURCE/rpc_client 192.168.16.135 service_1 "agent1 to service_1 from srvns1"
-ip netns exec ns1 rpcinfo -s
 
 ip netns exec ns2 ./CLI_SERV_SOURCE/rpc_server srvns2 service_2 tcp
 sleep 3
 ./CLI_SERV_SOURCE/rpc_client 192.168.16.136 service_2 "agent2 to service_2 from srvns2"
-ip netns exec ns2 rpcinfo -s
 
 ip netns exec ns1 ./CLI_SERV_SOURCE/rpc_server srvns3 service_3
 sleep 3
 ./CLI_SERV_SOURCE/rpc_client 192.168.16.135 service_3 tcp "agent3 to service_3 from srvns3"
-ip netns exec ns1 rpcinfo -s
 
 ./CLI_SERV_SOURCE/rpc_client 192.168.16.135 service_1 "agent1 to service_1 from srvns1"
 
