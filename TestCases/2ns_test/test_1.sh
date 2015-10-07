4d3e82c6f6a088ca12ea95c1778bd0342c117a2f
kseniya-buraya <ksu.nyaka@gmail.com>
2015-10-07 03:34:33 -0400

    test cases updete

---
+++ TestCases/2ns_test/test_1.sh
@@ -0,0 +1,71 @@
+#default configs
+#ns1 - 192.168.16.135
+#ns2 - 192.168.16.136
+#success test
+
+echo "Run service1 in NS 1, 2"
+ip netns exec ns1 ./CLI_SERV_SOURCE/rpc_server srv_ns1 service_1 tcp
+sleep 2
+ip netns exec ns2 ./CLI_SERV_SOURCE/rpc_server srv_ns2 service_1 tcp
+sleep 2
+
+echo "Test connections: ns1 - service1, ns2 - servive"
+./CLI_SERV_SOURCE/rpc_client 192.168.16.135 service_1 "client from ns1 - service_1"
+./CLI_SERV_SOURCE/rpc_client 192.168.16.136 service_1 "client from ns2 - service_1"
+
+#stop rpcbind in ns 1 and delete ns1
+echo "Stop rpcbind in ns1 and delete ns1"
+./Agents/nsconfig.sh ns1 stop
+ip link del brns1
+
+echo "Test connections: ns2 - service1"
+./CLI_SERV_SOURCE/rpc_client 192.168.16.136 service_1 "client from ns2 - service_1"
+
+echo "Start rpcbin in ns1"
+ip netns add ns1
+python Agents/vethconf.py -ns ns1 192.168.16.135
+./Agents/nsconfig.sh ns1 start
+
+echo "Start service2 in ns1"
+ip netns exec ns1 ./CLI_SERV_SOURCE/rpc_server srv_ns1 service_2 tcp
+sleep 2
+
+echo "Test connections: ns1 - service2, ns2 - service1"
+./CLI_SERV_SOURCE/rpc_client 192.168.16.135 service_2 "client from ns1 - service_2"
+./CLI_SERV_SOURCE/rpc_client 192.168.16.136 service_1 "client from ns2 - service_1"
+
+echo "Run service3 in ns1"
+ip netns exec ns1 ./CLI_SERV_SOURCE/rpc_server srv_ns1 service_3 tcp
+sleep 2
+
+echo "Test connections: ns1 - service2, ns1 - service3, ns2 - service1"
+./CLI_SERV_SOURCE/rpc_client 192.168.16.135 service_2 "client from ns1 - service_2"
+./CLI_SERV_SOURCE/rpc_client 192.168.16.135 service_3 "client from ns1 - service_3"
+./CLI_SERV_SOURCE/rpc_client 192.168.16.136 service_1 "client from ns2 - service_1"
+
+echo "Stop rpcbind in ns2 and delete ns2"
+./Agents/nsconfig.sh ns2 stop
+ip link del brns2
+
+echo "Test connections: ns1 - service2, ns2 - service3"
+./CLI_SERV_SOURCE/rpc_client 192.168.16.135 service_2 "client from ns1 - service_2"
+./CLI_SERV_SOURCE/rpc_client 192.168.16.135 service_2 "client from ns1 - service_3"
+
+echo "Start rpcbind in ns2"
+ip netns add ns2
+python Agents/vethconf.py -ns ns2 192.168.16.136
+./Agents/nsconfig.sh ns2 start
+
+echo "Run service2 in ns2"
+ip netns exec ns2 ./CLI_SERV_SOURCE/rpc_server srv_ns2 service_2 tcp 
+
+echo "Test connections: ns1 - service2, ns1 - service3, ns2 - service2"
+./CLI_SERV_SOURCE/rpc_client 192.168.16.135 service_2 "client from ns1 - service_2"
+./CLI_SERV_SOURCE/rpc_client 192.168.16.135 service_3 "client from ns1 - service_3"
+./CLI_SERV_SOURCE/rpc_client 192.168.16.136 service_2 "client from ns2 - service_2"
+
+#stop
+echo "End test, stop everything"
+./Agents/nsconfig.sh ns1 stop
+./Agents/nsconfig.sh ns2 stop
+
--- TestCases/3ns_test/test_1.sh
+++ TestCases/3ns_test/test_1.sh
@@ -4,8 +4,7 @@
 #ns3 - 192.168.16.137
 
 
-#run service_1 in each ns
-echo "Start service_1 in ns1,2,3"
+echo "Start service1 in ns1,2,3"
 ip netns exec ns1 ./CLI_SERV_SOURCE/rpc_server srv_ns1 service_1 tcp
 sleep 2
 ip netns exec ns2 ./CLI_SERV_SOURCE/rpc_server srv_ns2 service_1 tcp
@@ -14,83 +13,71 @@ ip netns exec ns3 ./CLI_SERV_SOURCE/rpc_server srv_ns3 service_1 tcp
 sleep 2
 
 
-#test connection
-echo "Test connection in ns1,2,3,of service_1"
+echo "Test connections: ns1 - service1, ns2 - service1, ns3 - service1"
 ./CLI_SERV_SOURCE/rpc_client 192.168.16.135 service_1 "client from ns1 - service_1"
 ./CLI_SERV_SOURCE/rpc_client 192.168.16.136 service_1 "client from ns2 - service_1"
 ./CLI_SERV_SOURCE/rpc_client 192.168.16.137 service_1 "client from ns3 - service_1"
 
 echo "Stop ns1"
-#stop rpcbind in ns1
 ./Agents/nsconfig.sh ns1 stop
 ip link del brns1
 
-echo "Test connection in ns2, 3, of service_1"
-#test connection
+echo "Test connections: ns2 - service1, ns3 - service1"
 ./CLI_SERV_SOURCE/rpc_client 192.168.16.136 service_1 "client from ns2 - service_1"
 ./CLI_SERV_SOURCE/rpc_client 192.168.16.137 service_1 "client from ns3 - service_1"
 
 echo "Add ns1 + run rpcbind"
-#add ns1
 ip netns add ns1
 python Agents/vethconf.py -ns ns1 192.168.16.135
 ./Agents/nsconfig.sh ns1 start
 
-echo "run service_2 in ns1"
-#run service_2 in ns1
+echo "Run service_2 in ns1"
 ip netns exec ns1 ./CLI_SERV_SOURCE/rpc_server srv_ns1 service_2 tcp
 sleep 2
 
-echo "test connection in ns1 of service_2"
-#test connection
+echo "Test connections: ns1 - service2, ns2 - service1, ns3 - service1"
 ./CLI_SERV_SOURCE/rpc_client 192.168.16.135 service_2 "client from ns1 - service_2"
+./CLI_SERV_SOURCE/rpc_client 192.168.16.136 service_1 "client from ns2 - service_1"
+./CLI_SERV_SOURCE/rpc_client 192.168.16.137 service_1 "client from ns3 - service_1"
 
-echo "run service_3 in ns1"
-#run service_3 in ns1
+echo "Run service_3 in ns1"
 ip netns exec ns1 ./CLI_SERV_SOURCE/rpc_server srv_ns1 service_3 tcp
 sleep 2
-echo "test connection in ns2 of service_3"
-#test connection
-./CLI_SERV_SOURCE/rpc_client 192.168.16.135 service_2 "client from ns1 - service_3"
+
+echo "Test connections: ns1 - service2, ns1 - service3, ns2 - service1, ns3 - service1"
+./CLI_SERV_SOURCE/rpc_client 192.168.16.135 service_2 "client from ns1 - service_2"
+./CLI_SERV_SOURCE/rpc_client 192.168.16.135 service_3 "client from ns1 - service_3"
+./CLI_SERV_SOURCE/rpc_client 192.168.16.136 service_1 "client from ns2 - service_1"
+./CLI_SERV_SOURCE/rpc_client 192.168.16.137 service_1 "client from ns3 - service_1"
 
 
-echo "stop ns2"
-#stop rpcbind in ns2
+echo "Stop ns2"
 ./Agents/nsconfig.sh ns2 stop
 ip link del brns2
 
-echo "test connection: ns1 service 2,3; ns3 service 1"
+echo "Test connections: ns1 - service2, ns1 - service3, ns3 - service1"
 ./CLI_SERV_SOURCE/rpc_client 192.168.16.135 service_2 "client from ns1 - service_2"
 ./CLI_SERV_SOURCE/rpc_client 192.168.16.135 service_3 "client from ns1 - service_3"
-./CLI_SERV_SOURCE/rpc_client 192.168.16.137 service_1 "client from ns3 - service_3"
+./CLI_SERV_SOURCE/rpc_client 192.168.16.137 service_1 "client from ns3 - service_1"
 
-echo "start ns2 + rpcbind"
-#add ns2
+echo "Add ns2 + run rpcbind"
 ip netns add ns2
 python Agents/vethconf.py -ns ns2 192.168.16.136
 ./Agents/nsconfig.sh ns2 start
 
-#SLES 12 CRASHES HERE (can't register serice error)
 
-echo "run service_1 in ns2"
-#run service_1 in ns2
-./CLI_SERV_SOURCE/rpc_server srv_ns2 service_1 tcp
+echo "Run service_1 in ns2"
+ip netns exec ns2 ./CLI_SERV_SOURCE/rpc_server srv_ns2 service_1 tcp
 sleep 2
 
-echo "test connections"
-#check all connections
+echo "Test connections: ns1 - service2, ns1 - service3, ns2 - service2, ns3 - service1"
 ./CLI_SERV_SOURCE/rpc_client 192.168.16.135 service_2 "client from ns1 - service_2"
 ./CLI_SERV_SOURCE/rpc_client 192.168.16.135 service_3 "client from ns1 - service_3"
-
-
-#SLES 11 CRASHES HERE (can't access service error)
 ./CLI_SERV_SOURCE/rpc_client 192.168.16.136 service_1 "client from ns2 - service_1"
-
 ./CLI_SERV_SOURCE/rpc_client 192.168.16.137 service_1 "client from ns3 - service_1"
 
-
 echo "End test"
-#end test
+
 pkill rpc_server
 ./Agents/nsconfig.sh ns1 stop
 ./Agents/nsconfig.sh ns2 stop
