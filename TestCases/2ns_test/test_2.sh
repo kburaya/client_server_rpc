ed3f0671f65787c72fa2b4e7fd9d8f748411935e
kseniya-buraya <ksu.nyaka@gmail.com>
2015-10-08 09:19:47 -0400

    test changes

--- TestCases/2ns_test/test_2.sh
+++ TestCases/2ns_test/test_2.sh
@@ -6,39 +6,25 @@
 python Agents/StorageArray.py -c Configs/StorageArray_2ns.xml
 
 
-echo "Run service1 in NS 1, 2"
+echo "Run service1 in NS 1, service2 in NS2"
 ip netns exec ns1 ./CLI_SERV_SOURCE/rpc_server srv_ns1 service_1 $1
 sleep 2
-ip netns exec ns2 ./CLI_SERV_SOURCE/rpc_server srv_ns2 service_1 $1
+ip netns exec ns2 ./CLI_SERV_SOURCE/rpc_server srv_ns2 service_2 $1
 sleep 2
 
 echo "Test connections: ns1 - service1, ns2 - service1"
 ./CLI_SERV_SOURCE/rpc_client 192.168.16.135 service_1 "client from ns1 - service_1"
-./CLI_SERV_SOURCE/rpc_client 192.168.16.136 service_1 "client from ns2 - service_1"
-
-echo "Run service2 in NS 1"
-ip netns exec ns1 ./CLI_SERV_SOURCE/rpc_server srv_ns1_1 service_2 $1
-sleep 2
-
-echo "Test connections: ns1 - service1, ns2 - service1"
-./CLI_SERV_SOURCE/rpc_client 192.168.16.135 service_1 "client from ns1 - service_1"
-./CLI_SERV_SOURCE/rpc_client 192.168.16.136 service_1 "client from ns2 - service_1"
-echo "Test connections: ns1 - service2"
-./CLI_SERV_SOURCE/rpc_client 192.168.16.135 service_2 "client from ns1 - service_2"
-
+./CLI_SERV_SOURCE/rpc_client 192.168.16.136 service_1 "client from ns2 - service_2"
 
 echo "Run service3 in NS 1"
-ip netns exec ns1 ./CLI_SERV_SOURCE/rpc_server srv_ns1_2 service_3 $1
+ip netns exec ns1 ./CLI_SERV_SOURCE/rpc_server srv_ns1_1 service_3 $1
 sleep 2
 
-
-echo "Test connections: ns1 - service1, ns2 - service1"
+echo "Test connections: ns1 - service1, ns2 - service2"
 ./CLI_SERV_SOURCE/rpc_client 192.168.16.135 service_1 "client from ns1 - service_1"
-./CLI_SERV_SOURCE/rpc_client 192.168.16.136 service_1 "client from ns2 - service_1"
-echo "Test connections: ns1 - service2"
-./CLI_SERV_SOURCE/rpc_client 192.168.16.135 service_2 "client from ns1 - service_2"
+./CLI_SERV_SOURCE/rpc_client 192.168.16.136 service_2 "client from ns2 - service_2"
 echo "Test connections: ns1 - service3"
-./CLI_SERV_SOURCE/rpc_client 192.168.16.135 service_2 "client from ns1 - service_2"
+./CLI_SERV_SOURCE/rpc_client 192.168.16.135 service_3 "client from ns1 - service_3"
 
 
 #stop
