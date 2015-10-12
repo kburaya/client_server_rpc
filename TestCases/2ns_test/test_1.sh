c7670cf736974dab094c2fc987f2bf0d6b990856
kseniya-buraya <ksu.nyaka@gmail.com>
2015-10-12 10:13:38 -0400

    readme changes

--- TestCases/2ns_test/test_1.sh
+++ TestCases/2ns_test/test_1.sh
@@ -5,24 +5,34 @@
 
 python Agents/StorageArray.py -c Configs/StorageArray_2ns.xml
 
+echo "STEP 1"
 echo "Run service1 in NS 1, 2"
 ip netns exec ns1 ./CLI_SERV_SOURCE/rpc_server srv_ns1 service_1 $1
 sleep 2
 ip netns exec ns2 ./CLI_SERV_SOURCE/rpc_server srv_ns2 service_1 $1
 sleep 2
 
-echo "Test connections: ns1 - service1, ns2 - servive"
-./CLI_SERV_SOURCE/rpc_client 192.168.16.135 service_1 "client from ns1 - service_1"
-./CLI_SERV_SOURCE/rpc_client 192.168.16.136 service_1 "client from ns2 - service_1"
+echo "Rpcinfo in ns1 output"
+ip netns exec ns1 rpcinfo -s
+echo "Rpcinfo in ns2 output"
+ip netns exec ns2 rpcinfo -s
 
+echo "Test connections: ns1 - service1, ns2 - servive"
+./CLI_SERV_SOURCE/rpc_client 192.168.16.135 service_1 "client from ns1 - service_1" $2
+./CLI_SERV_SOURCE/rpc_client 192.168.16.136 service_1 "client from ns2 - service_1" $2
+
+
+
+echo "STEP 2"
 #stop rpcbind in ns 1 and delete ns1
 echo "Stop rpcbind in ns1 and delete ns1"
 ./Agents/nsconfig.sh ns1 stop
 ip link del brns1
 
 echo "Test connections: ns2 - service1"
-./CLI_SERV_SOURCE/rpc_client 192.168.16.136 service_1 "client from ns2 - service_1"
+./CLI_SERV_SOURCE/rpc_client 192.168.16.136 service_1 "client from ns2 - service_1" $2
 
+echo "STEP 3"
 echo "Start rpcbin in ns1"
 ip netns add ns1
 python Agents/vethconf.py -ns ns1 192.168.16.135
@@ -32,39 +42,69 @@ echo "Start service2 in ns1"
 ip netns exec ns1 ./CLI_SERV_SOURCE/rpc_server srv_ns1 service_2 $1
 sleep 2
 
-echo "Test connections: ns1 - service2, ns2 - service1"
-./CLI_SERV_SOURCE/rpc_client 192.168.16.135 service_2 "client from ns1 - service_2"
-./CLI_SERV_SOURCE/rpc_client 192.168.16.136 service_1 "client from ns2 - service_1"
+echo "Rpcinfo in ns1 output"
+ip netns exec ns1 rpcinfo -s
+echo "Rpcinfo in ns2 output"
+ip netns exec ns2 rpcinfo -s
 
+echo "Test connections: ns1 - service2, ns2 - service1"
+./CLI_SERV_SOURCE/rpc_client 192.168.16.135 service_2 "client from ns1 - service_2" $2
+./CLI_SERV_SOURCE/rpc_client 192.168.16.136 service_1 "client from ns2 - service_1" $2
+
+echo "Rpcinfo in ns1 output"
+ip netns exec ns1 rpcinfo -s
+echo "Rpcinfo in ns2 output"
+ip netns exec ns2 rpcinfo -s
+
+echo "STEP 4"
 echo "Run service3 in ns1"
 ip netns exec ns1 ./CLI_SERV_SOURCE/rpc_server srv_ns1 service_3 $1
 sleep 2
+echo "Rpcinfo in ns1 output"
+ip netns exec ns1 rpcinfo -s
+echo "Rpcinfo in ns2 output"
+ip netns exec ns2 rpcinfo -s
 
 echo "Test connections: ns1 - service2, ns1 - service3, ns2 - service1"
-./CLI_SERV_SOURCE/rpc_client 192.168.16.135 service_2 "client from ns1 - service_2"
-./CLI_SERV_SOURCE/rpc_client 192.168.16.135 service_3 "client from ns1 - service_3"
-./CLI_SERV_SOURCE/rpc_client 192.168.16.136 service_1 "client from ns2 - service_1"
+./CLI_SERV_SOURCE/rpc_client 192.168.16.135 service_2 "client from ns1 - service_2" $2
+./CLI_SERV_SOURCE/rpc_client 192.168.16.135 service_3 "client from ns1 - service_3" $2
+./CLI_SERV_SOURCE/rpc_client 192.168.16.136 service_1 "client from ns2 - service_1" $2
 
+
+echo "STEP 4"
 echo "Stop rpcbind in ns2 and delete ns2"
 ./Agents/nsconfig.sh ns2 stop
 ip link del brns2
 
-echo "Test connections: ns1 - service2, ns2 - service3"
-./CLI_SERV_SOURCE/rpc_client 192.168.16.135 service_2 "client from ns1 - service_2"
-./CLI_SERV_SOURCE/rpc_client 192.168.16.135 service_2 "client from ns1 - service_3"
+echo "Rpcinfo in ns1 output"
+ip netns exec ns1 rpcinfo -s
+echo "Rpcinfo in ns2 output"
+ip netns exec ns2 rpcinfo -s
 
+echo "Test connections: ns1 - service2, ns2 - service3"
+./CLI_SERV_SOURCE/rpc_client 192.168.16.135 service_2 "client from ns1 - service_2" $2
+./CLI_SERV_SOURCE/rpc_client 192.168.16.135 service_2 "client from ns1 - service_3" $2
+
+
+
+echo "STEP 5"
 echo "Start rpcbind in ns2"
 ip netns add ns2
 python Agents/vethconf.py -ns ns2 192.168.16.136
 ./Agents/nsconfig.sh ns2 start
 
 echo "Run service2 in ns2"
-ip netns exec ns2 ./CLI_SERV_SOURCE/rpc_server srv_ns2 service_2 $1 
+ip netns exec ns2 ./CLI_SERV_SOURCE/rpc_server srv_ns2 service_2 $1
+
+echo "Rpcinfo in ns1 output"
+ip netns exec ns1 rpcinfo -s
+echo "Rpcinfo in ns2 output"
+ip netns exec ns2 rpcinfo -s 
 
 echo "Test connections: ns1 - service2, ns1 - service3, ns2 - service2"
-./CLI_SERV_SOURCE/rpc_client 192.168.16.135 service_2 "client from ns1 - service_2"
-./CLI_SERV_SOURCE/rpc_client 192.168.16.135 service_3 "client from ns1 - service_3"
-./CLI_SERV_SOURCE/rpc_client 192.168.16.136 service_2 "client from ns2 - service_2"
+./CLI_SERV_SOURCE/rpc_client 192.168.16.135 service_2 "client from ns1 - service_2" $2
+./CLI_SERV_SOURCE/rpc_client 192.168.16.135 service_3 "client from ns1 - service_3" $2
+./CLI_SERV_SOURCE/rpc_client 192.168.16.136 service_2 "client from ns2 - service_2" $2
 
 #stop
 echo "End test, stop everything"
--- TestCases/2ns_test/test_2.sh
+++ TestCases/2ns_test/test_2.sh
@@ -3,6 +3,11 @@
 #ns2 - 192.168.16.136
 #success test
 
+#service numbers
+#service_1 536870913
+#service_2 536870914
+#service_3 536870915
+
 python Agents/StorageArray.py -c Configs/StorageArray_2ns.xml
 
 
@@ -13,18 +18,18 @@ ip netns exec ns2 ./CLI_SERV_SOURCE/rpc_server srv_ns2 service_2 $1
 sleep 2
 
 echo "Test connections: ns1 - service1, ns2 - service1"
-./CLI_SERV_SOURCE/rpc_client 192.168.16.135 service_1 "client from ns1 - service_1"
-./CLI_SERV_SOURCE/rpc_client 192.168.16.136 service_1 "client from ns2 - service_2"
+./CLI_SERV_SOURCE/rpc_client 192.168.16.135 service_1 "client from ns1 - service_1" $2
+./CLI_SERV_SOURCE/rpc_client 192.168.16.136 service_2 "client from ns2 - service_2" $2
 
 echo "Run service3 in NS 1"
 ip netns exec ns1 ./CLI_SERV_SOURCE/rpc_server srv_ns1_1 service_3 $1
 sleep 2
 
 echo "Test connections: ns1 - service1, ns2 - service2"
-./CLI_SERV_SOURCE/rpc_client 192.168.16.135 service_1 "client from ns1 - service_1"
-./CLI_SERV_SOURCE/rpc_client 192.168.16.136 service_2 "client from ns2 - service_2"
+./CLI_SERV_SOURCE/rpc_client 192.168.16.135 service_1 "client from ns1 - service_1" $2
+./CLI_SERV_SOURCE/rpc_client 192.168.16.136 service_2 "client from ns2 - service_2" $2
 echo "Test connections: ns1 - service3"
-./CLI_SERV_SOURCE/rpc_client 192.168.16.135 service_3 "client from ns1 - service_3"
+./CLI_SERV_SOURCE/rpc_client 192.168.16.135 service_3 "client from ns1 - service_3" $2
 
 
 #stop
